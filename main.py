b5962eac1d16d4e10277d1d9f748509a2b1fa860
Parth Harpal <parthharpal37@gmail.com>
2026-05-19 18:21:45 +0530

    Alembic setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+def main():
+    print("Hello from user-profile-api!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import Integer,String,Boolean,Text,null
+
+class Base(DeclarativeBase):
+    pass
